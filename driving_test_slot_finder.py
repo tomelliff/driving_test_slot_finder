225e251718c5014723cafea7bab79eaf18625ead
Tom Elliff <tomelliff@gmail.com>
2015-10-18 16:22:09 +0000

    First attempt at using sessions

--- driving_test_slot_finder.py
+++ driving_test_slot_finder.py
@@ -7,10 +7,24 @@ import requests
 a='TEST'
 b='123456'
 
-def login(driving_licence_num, application_ref_num):
+def create_session():
+  
+  # Creates a session to be used throughout
+
+  login_url = 'https://driverpracticaltest.direct.gov.uk/login'
+  
+  s = requests.Session()
+
+  s.get(login_url)
+
+  return s 
+
+def login(session, driving_licence_num, application_ref_num):
 
   # Logs in to the driving test management portal
   # with provided credentials
+
+  s = session
 
   login_url = 'https://driverpracticaltest.direct.gov.uk/login'
   login_details = {
@@ -27,10 +41,15 @@ def login(driving_licence_num, application_ref_num):
   form_data = login_details.copy()
   form_data.update(other_form_data)
 
-  r = requests.post(login_url, data=form_data)
+  r = s.post(login_url, data=form_data)
 
   return r
 
-login = login(a,b)
+#cookies = create_session().cookies
+
+session = create_session()
+login = login(session,a,b)
 
 print(login.text)
+print('')
+print(session.cookies)
