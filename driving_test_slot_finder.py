8f563a720c660d63821c6e687faa26406b7ed0a0
Tom Elliff <tomelliff@gmail.com>
2015-10-18 18:03:35 +0000

    Check for captcha challenge

--- driving_test_slot_finder.py
+++ driving_test_slot_finder.py
@@ -4,7 +4,9 @@
 
 import ConfigParser
 import re
+import sys
 
+from bs4 import BeautifulSoup
 import requests
 
 config = ConfigParser.RawConfigParser()
@@ -16,19 +18,22 @@ application_ref_num = config.get('credentials', 'application_reference_number')
 def create_session():
   
   # Creates a session to be used throughout
+  # Returns session object and boolean capctcha_present
 
   login_url = 'https://driverpracticaltest.direct.gov.uk/login'
   
   s = requests.Session()
 
-  s.get(login_url)
+  r = s.get(login_url)
 
-  return s 
+  captcha_present = check_for_captcha(r.text)
+  
+  return s, captcha_present
 
 def login(session, driving_licence_num, application_ref_num):
 
-  # Logs in to the driving test management portal
-  # with provided credentials
+  # Logs in to the driving test management portal with provided credentials
+  # Returns request object
 
   s = session
 
@@ -51,15 +56,36 @@ def login(session, driving_licence_num, application_ref_num):
 
   return r
 
-#cookies = create_session().cookies
+def check_for_captcha(html):
 
-session = create_session()
-login = login(session, driving_licence_num, application_ref_num)
-csrf_search = re.search('csrftoken=(.*)&amp', login.text)
+  # Checks for captcha element
+  # Returns boolean - True if recaptcha-check element is present
 
-print(login.text)
-print('')
-print(session.cookies)
-if csrf_search is not None:
-  csrf = csrf_search.group(1)
-  print(csrf)
+  html_soup = BeautifulSoup(html, 'html.parser')
+  captcha_challenge = html_soup.find(id='recaptcha-check')
+
+  if captcha_challenge is None:
+    captcha = False
+  else:
+    captcha = True
+
+  return captcha
+
+
+### Use functions
+
+session, captcha_present = create_session()
+
+# Only login if captcha not present on login page
+if not captcha_present:
+  login = login(session, driving_licence_num, application_ref_num)
+  print(login.text)
+  csrf_search = re.search('csrftoken=(.*)&amp', login.text)
+  if csrf_search is not None:
+    csrf = csrf_search.group(1)
+    print(csrf)
+  else:
+    print('CSRF token not found')
+else:
+  print('Captcha challenge detected, exiting')
+  sys.exit(1)
